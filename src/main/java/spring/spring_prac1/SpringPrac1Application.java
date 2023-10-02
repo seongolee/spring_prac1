77ddb4473cd8d8c2e8cabb0a8653a1e138e3341f
seongolee <seongo0521@gmail.com>
2023-10-02 22:00:27 +0900

    Init

---
+++ src/main/java/spring/spring_prac1/SpringPrac1Application.java
@@ -0,0 +1,13 @@
+package spring.spring_prac1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrac1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrac1Application.class, args);
+    }
+
+}
