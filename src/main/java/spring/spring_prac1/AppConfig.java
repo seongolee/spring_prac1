0a5bdf704e5c6a8808ba8ccc1e84f2a5b29783b9
seongolee <seongo0521@gmail.com>
2023-10-03 10:58:00 +0900

    AppConfig 리팩토링

--- src/main/java/spring/spring_prac1/AppConfig.java
+++ src/main/java/spring/spring_prac1/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.spring_prac1;
 
+import spring.spring_prac1.discount.DiscountPolicy;
 import spring.spring_prac1.discount.FixDiscountPolicy;
+import spring.spring_prac1.member.MemberRepository;
 import spring.spring_prac1.member.MemberService;
 import spring.spring_prac1.member.MemberServiceImpl;
 import spring.spring_prac1.member.MemoryMemberRepository;
@@ -9,14 +11,22 @@ import spring.spring_prac1.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
     }
 }
