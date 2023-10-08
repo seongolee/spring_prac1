da8c1780707ca7ab0b9a273b56f4ada56514e00f
seongolee <seongo0521@gmail.com>
2023-10-08 23:31:15 +0900

    xml로 빈등록 설정, 테스트 케이스 작성

---
+++ src/test/java/spring/spring_prac1/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package spring.spring_prac1.xml;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import spring.spring_prac1.member.MemberService;
+
+public class XmlAppContext {
+
+    @Test
+    @DisplayName("xml 파일로 빈등록 테스트")
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
