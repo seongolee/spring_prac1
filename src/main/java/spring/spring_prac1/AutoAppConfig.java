229886c78b7a4945a8ffe2e1eba4f6ed7339e5cd
seongolee <seongo0521@gmail.com>
2023-10-14 16:57:09 +0900

    ComponentScan 설명 수정

--- src/main/java/spring/spring_prac1/AutoAppConfig.java
+++ src/main/java/spring/spring_prac1/AutoAppConfig.java
@@ -9,11 +9,11 @@ import org.springframework.context.annotation.FilterType;
         // 컴포넌트를 읽을 시작 위치를 지정
         basePackages = "spring.spring_prac1",
 
-        // AutoAppConfig.class가 속한 package가 시작 위치로 지정된다.
+        // AutoAppConfig.class의 package 위치가 탐색 시작 위치로 지정된다.
         basePackageClasses = AutoAppConfig.class,
 
-        // basePackages, basePackageClasses 값이 지정이 되어있지 않으면 default 값으로 @Configuration을
-        // 선언한 AutoAppConfig.class의 package 경로가 시작 위치가 된다.
+        // basePackages, basePackageClasses 값이 지정이 되어있지 않으면 default 값으로 @Configuration가 붙은
+        // 설정정보 클래스의 package위치가 탐색 시작 위치가 된다.
 
         // 테스트를 위해서 Appconfig에 지정된 설정을 제외한다.
         excludeFilters = @ComponentScan.Filter(
